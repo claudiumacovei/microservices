a093f4d272e5d6b4728b406f0fe77a754689c369
Claudiu <Claudiu@claudiu-pc>
2017-07-27 21:37:20 +0300

    creating zuulproxy

---
+++ microservices_test/zuulproxy/src/main/java/org/zuulproxy/configuration/ZuulFilterConfiguration.java
@@ -0,0 +1,14 @@
+package org.zuulproxy.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zuulproxy.filters.RequestMonitoringFilter;
+
+@Configuration
+public class ZuulFilterConfiguration {
+
+	@Bean
+	public RequestMonitoringFilter requestMonitoringFilter() {
+		return new RequestMonitoringFilter();
+	}
+}
