a99e59624fd6599d96c7fb793277fa2b5968e8f3
Claudiu <Claudiu@claudiu-pc>
2017-07-27 21:23:51 +0300

    creating colors service

---
+++ microservices_test/colors/src/main/java/org/colors/controller/Controller.java
@@ -0,0 +1,17 @@
+package org.colors.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping
+	public List<String> available() {
+		return Arrays.asList("Red", "Blue");
+	}
+	
+}
