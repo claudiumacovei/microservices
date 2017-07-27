68b0ab9c6b96bccfe137421a657ca0e1854ac12a
Claudiu <Claudiu@claudiu-pc>
2017-07-27 21:42:10 +0300

    adding welcome message handler

---
+++ microservices_test/zuulproxy/src/main/java/org/zuulproxy/controller/Controller.java
@@ -0,0 +1,13 @@
+package org.zuulproxy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@RequestMapping
+	public String welcome() {
+		return "Welcome to zull proxy!";
+	}
+}
